a620cc26bbc3e4a7dc509e8c76c87f6de89ed652
kalandraka86 <hugogilb2005@gmail.com>
2025-12-22 15:04:48 +0100

    Diseño y desarrollo de panel de filtrado avanzado
    Creada la estética y implementación visual, falta obtención de datos para el filtrado y response de filtración avanzada

--- FrontEndESCREAM/src/app/components/movies/movies-list/movies-list.component.ts
+++ FrontEndESCREAM/src/app/components/movies/movies-list/movies-list.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SkeletonLoaderComponent } from "../../skeleton-loader/skeleton-loader.component";
 import { RouterLink } from '@angular/router';
 import { MovieService } from '../../../services/movie/movie.service';
+import { trigger, state, style, transition, animate, query } from '@angular/animations';
 
 @Component({
   selector: 'app-movies-list',
@@ -11,15 +12,39 @@ import { MovieService } from '../../../services/movie/movie.service';
   imports: [CommonModule, SkeletonLoaderComponent, RouterLink],
   templateUrl: './movies-list.component.html',
   styleUrls: ['./movies-list.component.css'],
+  animations: [
+    // Aninmación de despliegue del menú
+    trigger('filterMenuAnimation', [
+      state('hidden', style({
+        height: '0px',
+        opacity: 0,
+        transform: 'translateY(-10px)',
+        overflow: 'hidden'
+      })),
+      state('visible', style({
+        height: '*',
+        opacity: 1,
+        transform: 'translateY(0)',
+        margin: '10px 0 50px 0'
+      })),
+      transition('hidden => visible', animate('0.6s ease-out')),
+      transition('visible => hidden', animate('0.5s ease-in'))
+    ])
+  ]
 })
+
 export class MoviesListComponent {
   private service = inject(MovieService);
   public movies: Movie[] = [];
   public skeletons = new Array(6);
+  public showFilter: boolean = false;
 
   public currentPage = 1;
   public lastPage = 1;
   public perPage = 6;
+  public years = ["prueba", "prueba", "prueba"]
+  public countries = []
+  public genres = []
 
 
   ngOnInit() {
@@ -56,7 +81,27 @@ export class MoviesListComponent {
   public goToMovieDetail(movie: string) {
     console.log(movie)
   }
+
+  toggleFilter() {
+    this.showFilter = !this.showFilter;
+  }
+
+  //Control de listas desplegables
+
+  public dropdownOpen: { [key: string]: boolean } = {
+    year: false,
+    genre: false,
+    rating: false,
+    country: false
+  };
+
+  // Función para alternar cada dropdown
+  toggleDropdown(name: string) {
+    this.dropdownOpen[name] = !this.dropdownOpen[name];
+  }
 }
+
+
 
 
 // Opción moderna con callbacks, "Cuando se subscribe, se REGISTRA la función, y el Observable la ejecuta cuando EMITE"
